a8ea9d082bcc681f24b71a15199f08eaa41353c4
Maria Volpe <mariivo2000@gmail.com>
2025-05-30 17:26:44 +0200

    Create vector_store.py

---
+++ zero_shot_based/vector_store.py
@@ -0,0 +1,13 @@
+from langchain_community.vectorstores import Redis
+from redis import Redis
+
+embedding_model = SentenceTransformer("all-MiniLM-L6-v2")
+
+
+vector_store = RedisVectorStore(
+    index_name="Eu-demo",
+    embedding=SentenceTransformer("all-MiniLM-L6-v2"),
+    redis_url="redis://localhost:6379",
+)
+
+redis_client = redis.Redis(host="localhost", port=6379, decode_responses=False)
