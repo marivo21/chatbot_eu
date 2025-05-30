8e01cfdac4479d0249d03f7496adc0bee689ba8c
Maria Volpe <mariivo2000@gmail.com>
2025-05-30 17:29:57 +0200

    Update vector_store.py

--- zero_shot_based/vector_store.py
+++ zero_shot_based/vector_store.py
@@ -3,8 +3,9 @@ from redis import Redis
 from sentence_transformers import SentenceTransformer
 
 
-embeddings = emb_model.encode(texts) # restituisce una matrice NumPy
 
+emb_model = SentenceTransformer("all-MiniLM-L6-v2")
+embeddings = emb_model.encode(texts) # restituisce una matrice NumPy
 dimension = embeddings.shape[1] # la dimensione del vettore di embedding
 # index = faiss.IndexFlatL2(dimension) # distanza tra i vettori 
 # index.add(embeddings)
